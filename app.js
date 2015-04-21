f5be08a97b7073030b3712194c2b4961c07c0e14
Michael Erwin <michaeljameserwin@gmail.com>
2015-04-21 10:34:48 -0400

    Add db calls for register, login, and userlist

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
+// express setup
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     path = require('path'),
     port = process.env.PORT | 8080;
 
+// require dependencies
+var bodyParser = require('body-parser');
+
+// mongo setup
+var mongo = require('mongodb'),
+    db = require('monk')('localhost:27017/profile-app');
+
+// Setup public directory for static serving
 app.use(express.static(__dirname + 'public'));
+
+// Use bodyParser for POST requests
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// mongo setup
-// var MongoClient = require('mongodb').MongoClient();
-// MongoClient.connect('mongodb://localhost:27017/blog', function(err, db) {
-//     "use strict";
-//     if(err) throw err;
-// });
+// make db accessible to routes
+app.use(function(req,res,next) {
+    req.db = db;
+    next();
+});
 
 app.use('/', require('./routes'));
 
--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,54 @@ router.get('/login', function(req, res){
     res.render('login', title="Login");
 });
 
+router.post('/authorize', function(req,res){
+    var db = req.db,
+        users = db.get('users'),
+        login = req.body;
+    users.findOne({'username':login.username,'password':login.password},{},function(err, doc){
+        if(doc.username != login.username){
+            console.log("Username does not exist");
+        } else {
+            if(doc.password != login.password){
+                console.log("Incorrect password");
+            }
+            else{
+                res.redirect('/success');
+            }
+        }
+    });
+});
+
+router.get('/success', function(req, res){
+    res.send('You have successfully logged in!');
+});
+
 router.get('/register', function(req, res){
     res.render('register', title="Register");
 });
 
 router.post('/addUser', function(req, res){
+    var db = req.db,
+        users = db.get('users'),
+        register = req.body;
+    users.insert({'username': register.username, 'password': register.password, 'email': register.email}, function(err, inserted){
+        console.log(register.username + ' registered!');
+        res.redirect('/registered');
+    });
 
 });
 
+router.get('/registered', function(req, res){
+    res.send('You have successfully signed up. <a href="/login">Click here to login!</a>')
+});
+
 router.get('/users', function(req, res){
-    var userList = ['test1','test2','test3','test4'];
-    res.render('users', {title:"Users", userlist: userList});
+    var db = req.db,
+        users = db.get('users');
+
+    users.find({},{'username':1, '_id':0},function(err, docs){
+        res.render('users', {title:"Users", userlist: docs});
+    });
 });
 
 router.get('/users/:username', function(req, res){
